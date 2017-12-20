9ea758b2db8bf4e26a8c0ff79d1f8afa1c97e048
iamchuan <chuan.hong1101@gmail.com>
2017-12-20 02:22:56 -0500

    add month selection and pm 2.5 levels git pull

--- datahub_ap/app.py
+++ datahub_ap/app.py
@@ -1,19 +1,34 @@
-from flask import Flask, render_template
-from utils import DataWrangler
+from flask import Flask, render_template, request
+from utils import *
 
 app = Flask(__name__)
 
 data_path = './data/pm2p5_2013.csv'
 dw = DataWrangler(data_path)
-month = 2
+month_opt = {'January':1, 'February':2, 'March':3, 'April':4,
+             'May':5, 'June':6, 'July':7, 'August':8, 'September':9,
+             'October':10, 'November':11, 'December':12}
+
+default_mo = 1
 
 
-@app.route("/")
-def hello():
-    map_data = dw.get_month_data(month=month).to_dict(cols=['city', 'lat', 'lng', 'pm25'])
-    return render_template('index.html', map_data=map_data)
+@app.route("/", methods=['GET'])
+def china_map():
+    map_data = dw.get_month_data(month=default_mo)\
+        .add_col('category', value_to_category)\
+        .add_col('color', category_to_color)\
+        .to_dict(cols=['city', 'lat', 'lng', 'pm25', 'category', 'color'])
+    print(map_data)
+    return render_template('index.html', map_data=map_data, months=month_opt)
 
+
+@app.route("/<string:month>", methods=['GET'])
+def china_map_mon(month):
+    map_data = dw.get_month_data(month=month_opt[month])\
+        .add_col('category', value_to_category)\
+        .add_col('color', category_to_color)\
+        .to_dict(cols=['city', 'lat', 'lng', 'pm25', 'category', 'color'])
+    return render_template('index.html', map_data=map_data, months=month_opt, selected=month)
 
 if __name__ == "__main__":
-
     app.run(debug=True)
--- datahub_ap/utils.py
+++ datahub_ap/utils.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+category_map = {50:'Excellent',
+                100:'Good',
+                150:'Lightly Polluted',
+                200:'Moderately Polluted',
+                250:'Heavily Polluted',
+                float('inf'):'Severely Polluted'}
+
+color_map = {'Excellent':'#0b40e7',
+             'Good':'#11c300',
+             'Lightly Polluted':'#ffe132',
+             'Moderately Polluted':'#ff9936',
+             'Heavily Polluted':'#ff1313',
+             'Severely Polluted':'darkred'}
 
 class DataWrangler(object):
 
@@ -23,16 +36,30 @@ class DataWrangler(object):
         for _, row in self.data.iterrows():
             yield [row[col] for col in cols]
 
+    def add_col(self, col_name, func):
+        self.data[col_name] = self.data.apply(func=func, axis=1)
+        return self
+
     def to_dict(self, cols=None):
         if cols is None:
             return self.data.to_dict(orient='records')
         else:
             return self.data[cols].to_dict(orient='records')
 
+def value_to_category(row):
+    for val, level in sorted(category_map.items()):
+        if row['pm25'] <= val:
+            return level
+
+def category_to_color(row):
+    return color_map[row['category']]
 
 if __name__ == '__main__':
     data_path = './data/pm2p5_2013.csv'
     dw = DataWrangler(data_path)
     # for row in dw.to_list():
     #     print(row)
-    print(dw.get_month_data(2).to_dict())
+    print(dw.get_month_data(2)\
+          .add_col('category', value_to_category)\
+          .add_col('color', category_to_color)\
+          .to_dict())
