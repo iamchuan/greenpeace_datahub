5f564c41c4f2132bb5d7f1fd0565af8342045074
iamchuan <chuan.hong1101@gmail.com>
2018-01-04 16:06:29 -0500

    change backend to DB

--- datahub_ap/utils.py
+++ datahub_ap/utils.py
@@ -1,4 +1,7 @@
-import pandas as pd
+import pymysql.cursors
+import yaml
+from pymysql.err import OperationalError, InterfaceError
+
 
 category_map = {50:'Excellent',
                 100:'Good',
@@ -14,52 +17,82 @@ color_map = {'Excellent':'#0b40e7',
              'Heavily Polluted':'#ff1313',
              'Severely Polluted':'darkred'}
 
+
+class MySQLReader(object):
+
+    __CONFIG = None
+
+    def __init__(self, **kwargs):
+        self.__CONFIG = kwargs
+        self.__connect()
+
+    def __connect(self):
+        self.conn = pymysql.connect(charset="utf8mb4",
+                                    cursorclass=pymysql.cursors.DictCursor,
+                                    **self.__CONFIG)
+
+    def fetch_data(self, query):
+        while True:
+            try:
+                with self.conn.cursor() as cursor:
+                    cursor.execute(query)
+                    result = cursor.fetchall()
+                    return result
+            except (OperationalError, InterfaceError):
+                self.__connect()
+
+    def close(self):
+        self.conn.close()
+
+
 class DataWrangler(object):
 
-    def __init__(self, path=None, data=None):
-        if path is None:
-            if data is None:
-                raise ValueError('Cannot import data')
-            else:
-                self.data = data
-        else:
-            self.data = pd.read_csv(path)
+    data = []
 
-    def get_month_data(self, month):
-        return DataWrangler(data=self.data.loc[self.data['month'] == month])
+    def __init__(self, **kwargs):
+        self.reader = MySQLReader(**kwargs)
 
-    def to_list(self, cols=None, header=False):
-        if cols is None:
-            cols = list(self.data)
-        if header:
-            yield cols
-        for _, row in self.data.iterrows():
-            yield [row[col] for col in cols]
-
-    def add_col(self, col_name, func):
-        self.data[col_name] = self.data.apply(func=func, axis=1)
+    def get_month_data(self, year, month):
+        table = self.reader.fetch_data("""
+        SELECT ap.*, round(ll.latitude, 5) lat, round(ll.longitude, 5) lng FROM (
+            SELECT *
+            FROM map_air_pollution_{}
+            WHERE month = {}
+        ) ap INNER JOIN area_latlng ll
+        ON ap.area = ll.area
+        """.format(year, month))
+        self.data = table
         return self
 
-    def to_dict(self, cols=None):
-        if cols is None:
-            return self.data.to_dict(orient='records')
-        else:
-            return self.data[cols].to_dict(orient='records')
+    def add_col(self, col_name, func):
+        for row in self.data:
+            row[col_name] = func(row)
+        return self
+
+    def to_list(self, cols=None, header=False):
+        if header:
+            yield cols
+        for _, row in self.data:
+            yield [row[col] for col in cols]
+
 
 def value_to_category(row):
     for val, level in sorted(category_map.items()):
-        if row['pm25'] <= val:
+        if row['pm2_5'] <= val:
             return level
+
 
 def category_to_color(row):
     return color_map[row['category']]
 
+
 if __name__ == '__main__':
-    data_path = './data/pm2p5_2013.csv'
-    dw = DataWrangler(data_path)
-    # for row in dw.to_list():
-    #     print(row)
-    print(dw.get_month_data(2)\
-          .add_col('category', value_to_category)\
-          .add_col('color', category_to_color)\
-          .to_dict())
+
+    with open('./config.yml', 'r') as f:
+        CONFIG = yaml.load(f)
+    dw = DataWrangler(**CONFIG['mysql'])
+    print(dw.get_month_data(2013, 1)
+          .add_col('category', value_to_category)
+          .add_col('color', category_to_color)
+          .data)
+
